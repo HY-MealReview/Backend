684f302dbd653bf4cbfbf50a7c1579462418f6b8
redoyp <gsolu0302@gmail.com>
2024-10-31 23:48:52 +0900

    fix

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import site
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 application = get_wsgi_application()
